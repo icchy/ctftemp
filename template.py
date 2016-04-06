4df3510b7d573a4e60b288c5527d9861817f06fe
icchyr <icchyr@gmail.com>
2016-04-06 20:18:46 +0900

    using gdbserver with socat

---
+++ template.py
@@ -0,0 +1,35 @@
+from pwn import *
+
+context(os='linux', arch='{arch}')
+context.log_level = 'debug' # output verbose log
+
+RHOST = "{host}"
+RPORT = {port}
+LHOST = "127.0.0.1"
+LPORT = {port}
+
+# libc = ELF('{libc}')
+elf = ELF('{binary}')
+EntryPoint = elf.symbols['main'] or elf.entrypoint
+
+def section_addr(name, elf=elf):
+    return elf.get_section_by_name(name).header['sh_addr']
+
+conn = None
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'r':
+        conn = remote(RHOST, RPORT)
+    elif sys.argv[1] == 'l':
+        conn = remote(LHOST, LPORT)
+    elif sys.argv[1] == 'd':
+        conn = gdb.debug(['{binary}'], execute="b *"+hex(EntryPoint)+"\nc")
+        # conn = gdb.debug(['{binary}'], {gdbport}, env={{'LD_PRELOAD': '{libc}'}})
+else:
+    conn = process(['{binary}'])
+    # conn = process(['{binary}'], env={{'LD_PRELOAD': '{libc}'}})
+
+# preparing for exploitation
+
+log.info('Pwning')
+
+# conn.interactive()
---
+++ template_plain.py
@@ -0,0 +1,42 @@
+import socket
+import struct
+
+RHOST = "{host}"
+RPORT = {port}
+LHOST = "127.0.0.1"
+LPORT = {port}
+
+def remote(host, port):
+    return socket.create_connection((host, port))
+
+def process(*args):
+    import subprocess
+    p = subprocess.Popen(args, 
+            stdin=subprocess.PIPE.stdin,
+            stdout=subprocess.PIPE.stdout,
+            stderr=subprocess.PIPE.stderr)
+    return p.communicate()[0]
+
+def interact(s):
+    import telnetlib
+    t = telnetlib.Telnet()
+    t.sock = s
+    t.interact()
+
+p32 = lambda x: struct.pack('<I', x)
+u32 = lambda x: struct.unpack('<I', x)[0]
+
+conn = None
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'r':
+        conn = remote(RHOST, RPORT)
+    elif sys.argv[1] == 'l':
+        conn = remote(LHOST, LPORT)
+else:
+    conn = process(['{binary}'])
+    # conn = process(['{binary}'], env={{'LD_PRELOAD': '{libc}'}})
+
+# preparing for exploitation
+
+# interact()
